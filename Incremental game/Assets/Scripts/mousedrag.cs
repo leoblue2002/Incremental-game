5d7e34ef66d5f0f81ef6e78ba6b0b7297728d446
Lucas O'Connell <leoblue2002@gmail.com>
2019-03-20 13:08:59 -0400

    working sort of so far

---
+++ Incremental game/Assets/Scripts/mousedrag.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousedrag : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    public float movespeed = 100f;
+    private bool selected = false;
+    private bool letgo = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseEnter()
+    {
+        selected = true;  
+    }
+
+    private void OnMouseExit()
+    {
+        if (letgo)
+        {
+            selected = false;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && selected)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("mousePosition" + mousePosition);
+            Debug.Log("transform" + transform.position);
+            direction = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * movespeed, direction.y * movespeed);
+            letgo = false;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            letgo = true;
+        }
+    }
+}
