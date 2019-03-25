12e6090231aa66e0c08982c8da97cd548176a504
Lucas O'Connell <leoblue2002@gmail.com>
2019-03-25 13:11:50 -0400

    Money manager work and adding incenerator

---
+++ Incremental game/Assets/Scripts/moneymanager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moneymanager : MonoBehaviour
+{
+    public int Money;
+    public int MoneyPerSecond;
+    public int ActiveLvl1s;
+    public int ActiveLvl2s;
+    public int ActiveLvl3s
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("AddMoney", 1, 1);
+    }
+
+    void AddMoney ()
+    {
+        CalculateMoneyPerSecond();
+        Money += MoneyPerSecond;
+    }
+
+    void CalculateMoneyPerSecond ()
+    {
+        MoneyPerSecond = 1 * ActiveLvl1s + 10 * ActiveLvl2s;
+    }
+}
