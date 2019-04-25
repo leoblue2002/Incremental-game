20067320b7a4d7db616ffc8ecbce81aad6ea727e
Lucas O'Connell <leoblue2002@gmail.com>
2019-04-25 13:14:11 -0400

    Created a level 4 block

--- Incremental game/Assets/Scripts/CamraSlider.cs
+++ Incremental game/Assets/Scripts/CamraSlider.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CamraSlider : MonoBehaviour
 {
+    public float MaxUpwardRange;
+    private float MinLowerRange = 0.5f;
+
     public void MoveCamra (float yehaw)
     {
         Vector3 NiceVariableName = new Vector3(0,yehaw,-10);
@@ -14,6 +17,10 @@ public class CamraSlider : MonoBehaviour
     {
         Vector3 originalposition = transform.position;
         originalposition.y += yehaw;
+        if (originalposition.y < MinLowerRange)
+        { originalposition.y = MinLowerRange; }
+        if (originalposition.y > MaxUpwardRange)
+        { originalposition.y = MaxUpwardRange; }
         transform.position = originalposition;
     }
 
