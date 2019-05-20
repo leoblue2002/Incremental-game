52f7567995be8bdb2a67c0f5a8817ece26af8f09
Lucas O'Connell <leoblue2002@gmail.com>
2019-05-20 13:20:25 -0400

    save system work!

--- Incremental game/Assets/Scripts/GameSaver.cs
+++ Incremental game/Assets/Scripts/GameSaver.cs
@@ -1,25 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameSaver : MonoBehaviour
 {
     moneymanager MMRef;
 
-    public void SaveEverything ()
+
+    public void SaveEverything()
     {
         GameObject[] Blocks = GameObject.FindGameObjectsWithTag("Block");
         GameObject[] Platforms = GameObject.FindGameObjectsWithTag("Platform");
-
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
     }
+
+    // 46:35 of the video, Live Training 3 mar 2014 - data persistene
+
+    public void LoadThatBitch ()
+    {
+        if (File.Exists(Application.persistentDataPath + "playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter;
+            FileStream file = File.Open(Application.persistentDataPath + "playerInfo.dat",FileMode.Open);
+        }
+    }
+
 }
 
-[System.Serializable]
-class SaveObject
+[Serializable]
+class SaveBlock
 {
     float x;
     float y;
     float z;
     Quaternion rotation;
 
+}
+
+[Serializable]
+class SaveMoneyInfo
+{
+    decimal Money;
+    decimal[] CostOfBoxes;
+    decimal[] OwnedBoxes;
+    decimal CostOfNewPlatform;
 }
