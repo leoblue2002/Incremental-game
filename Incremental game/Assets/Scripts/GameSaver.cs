4ad832c5d1eecc2433e88de2bd02bd68fd626cfa
Lucas O'Connell <leoblue2002@gmail.com>
2019-05-22 13:12:49 -0400

    saving stuff progress and everything dissapeared for a second

--- Incremental game/Assets/Scripts/GameSaver.cs
+++ Incremental game/Assets/Scripts/GameSaver.cs
@@ -9,43 +9,84 @@ public class GameSaver : MonoBehaviour
 {
     moneymanager MMRef;
 
+    private void Start()
+    {
+        MMRef = GameObject.FindWithTag("MoneyManager").GetComponent<moneymanager>();
+    }
 
     public void SaveEverything()
     {
         GameObject[] Blocks = GameObject.FindGameObjectsWithTag("Block");
         GameObject[] Platforms = GameObject.FindGameObjectsWithTag("Platform");
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+        FileStream file = File.Create(Application.persistentDataPath + "/MoneyInfo.dat");
+
+        SaveMoneyInfo HitThatMFLike = new SaveMoneyInfo();
+        HitThatMFLike.Money = MMRef.Money;
+        HitThatMFLike.CostOfBoxes = MMRef.CostOfBoxes;
+        HitThatMFLike.CostOfNewPlatform = MMRef.CostOfNewPlatform;
+
+        bf.Serialize(file, HitThatMFLike);
+        file.Close();
+
     }
 
     // 46:35 of the video, Live Training 3 mar 2014 - data persistene
 
     public void LoadThatBitch ()
     {
-        if (File.Exists(Application.persistentDataPath + "playerInfo.dat"))
+        if (File.Exists(Application.persistentDataPath + "/MoneyInfo.dat"))
         {
-            BinaryFormatter bf = new BinaryFormatter;
-            FileStream file = File.Open(Application.persistentDataPath + "playerInfo.dat",FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/MoneyInfo.dat",FileMode.Open);
+            SaveMoneyInfo HTMFL = (SaveMoneyInfo)bf.Deserialize(file);
+            file.Close();
+            MMRef.Money = HTMFL.Money;
+            MMRef.CostOfBoxes = HTMFL.CostOfBoxes;
+            MMRef.OwnedBoxes = HTMFL.OwnedBoxes;
+            MMRef.CostOfNewPlatform = HTMFL.CostOfNewPlatform;
         }
     }
 
 }
 
 [Serializable]
+class SaveBlocks
+{
+    SaveBlock[] blocks;
+
+
+}
+
+
+[Serializable]
 class SaveBlock
 {
-    float x;
-    float y;
-    float z;
+    Vector3 BlockPos;
     Quaternion rotation;
+    int level;
 
+    public SaveBlock(Vector3 pos, Quaternion rot, int lvl)
+    {
+        BlockPos = pos;
+        rotation = rot;
+        level = lvl;
+    }
 }
 
 [Serializable]
 class SaveMoneyInfo
 {
-    decimal Money;
-    decimal[] CostOfBoxes;
-    decimal[] OwnedBoxes;
-    decimal CostOfNewPlatform;
+    public decimal Money;
+    public decimal[] CostOfBoxes;
+    public int[] OwnedBoxes;
+    public decimal CostOfNewPlatform;
+
+    //public SaveMoneyInfo (decimal inMoney, decimal[] inCostofBoxes, decimal[] InOwnedBoxes, decimal inCostOfnewPlatform)
+    //{
+    //    Money = inMoney;
+    //    CostOfBoxes = inCostofBoxes;
+    //    OwnedBoxes = InOwnedBoxes;
+    //    CostOfNewPlatform = inCostOfnewPlatform;
+    //}
 }
